3d6edc734222da6b895c6d7b79cd898c0254c143
abshrimp <abshrimps@gmail.com>
2026-07-05 02:31:35 +0900

    test

--- src/main/java/com/werewolf/utils/GameCommand.java
+++ src/main/java/com/werewolf/utils/GameCommand.java
@@ -3,9 +3,11 @@ package com.werewolf.utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 /**
@@ -31,12 +33,27 @@ public class GameCommand implements CommandExecutor {
         String name = command.getName().toLowerCase();
         switch (name) {
             case "wwstart" -> {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "function werewolf:game/start");
+                runStartInSenderOrConfiguredWorld(sender);
                 sender.sendMessage(Component.text("[WerewolfUtils] ").color(NamedTextColor.GOLD)
                     .append(Component.text("ゲームを開始しました").color(NamedTextColor.YELLOW)));
             }
             case "wwend" -> {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "function werewolf:game/end");
+                if (hasGameManager()) {
+                    Bukkit.dispatchCommand(
+                        Bukkit.getConsoleSender(),
+                        "execute as @e[type=minecraft:marker,tag=ww_game_manager,limit=1] at @s run function werewolf:game/end"
+                    );
+                } else {
+                    World world = resolveConfiguredWorld();
+                    if (world != null) {
+                        Bukkit.dispatchCommand(
+                            Bukkit.getConsoleSender(),
+                            "execute in " + world.getKey().asString() + " run function werewolf:game/end"
+                        );
+                    } else {
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "function werewolf:game/end");
+                    }
+                }
                 sender.sendMessage(Component.text("[WerewolfUtils] ").color(NamedTextColor.GOLD)
                     .append(Component.text("ゲームを終了しました").color(NamedTextColor.YELLOW)));
             }
@@ -45,5 +62,48 @@ public class GameCommand implements CommandExecutor {
             }
         }
         return true;
+    }
+
+    private void runStartInSenderOrConfiguredWorld(CommandSender sender) {
+        if (sender instanceof Player player) {
+            Bukkit.dispatchCommand(
+                Bukkit.getConsoleSender(),
+                "execute as " + player.getName() + " at @s run function werewolf:game/start"
+            );
+            return;
+        }
+
+        World world = resolveConfiguredWorld();
+        if (world != null) {
+            Bukkit.dispatchCommand(
+                Bukkit.getConsoleSender(),
+                "execute in " + world.getKey().asString() + " run function werewolf:game/start"
+            );
+        } else {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "function werewolf:game/start");
+        }
+    }
+
+    private boolean hasGameManager() {
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntities()) {
+                if (entity.getScoreboardTags().contains("ww_game_manager")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private World resolveConfiguredWorld() {
+        String configuredWorld = plugin.getConfig().getString("game-world", "world");
+        World world = Bukkit.getWorld(configuredWorld);
+        if (world != null) {
+            return world;
+        }
+        if (!Bukkit.getWorlds().isEmpty()) {
+            return Bukkit.getWorlds().get(0);
+        }
+        return null;
     }
 }
